7977b825feae5fd88cb012d620c50030cf1c39f1
yihaoh <yihaoh@bu.edu>
2021-09-13 01:32:00 -0400

    finish init script to install postgres and create database

--- init.sh
+++ init.sh
@@ -2,10 +2,13 @@
 sudo apt update
 sudo apt install postgresql postgresql-contrib
 
+sudo -u postgres psql postgres -c '\password'
+
+
 dbname=amazon
 
-if [[ -n `psql -lqt | cut -d \| -f 1 | grep -w "$dbname"` ]]; then
-    dropdb $dbname
+if [[ -n `sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -w "$dbname"` ]]; then
+    sudo -u postgres dropdb $dbname
 fi
-createdb $dbname
+sudo -u postgres createdb $dbname
 
