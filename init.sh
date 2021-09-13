fecfff4020c84e7d867ed49cb08db9d5f9ca3a5b
yihaoh <yihaoh@bu.edu>
2021-09-13 01:53:22 -0400

    add virtualenv to init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,13 @@
 sudo apt update
 sudo apt install postgresql postgresql-contrib
 
+# Uncomment this if using venv
+# sudo apt install python3.8-venv
+# python3 -m venv env
+# source env/bin/activate
+# pip install -r requirements.txt
+
+# This process is interactive, you need to enter a password same as the one in config.py
 sudo -u postgres psql postgres -c '\password'
 
 
