869b2cd07773bb7a2e36f84bfb9cd1dc3c1357b4
Rickard Stureborg <rs541@duke.edu>
2021-09-15 00:47:51 -0400

    reformatted query output to nice printable tables

--- app/routes/index.py
+++ app/routes/index.py
@@ -11,10 +11,13 @@ bp = Blueprint('index', __name__)
 def index():
     # get all the products for sale
     products = app.dbq.get_avail_products()
+    products_list = [app.dbq.get_product_details(x[0])[0] for x in products]
+    products_and_price = [(x[3],x[1]) for x in products_list]
     # find the products current user has bought
     purchases = app.dbq.get_purchase_history(current_user.id, datetime.datetime(1980,9,14,0,0,0))
-    #purchased_products = [db.get_product_details(x) for x in purchases]
+    purchase_list = [app.dbq.get_product_details(x[0])[0] for x in purchases]
+    purchase_name_and_price = [(x[3],x[1]) for x in purchase_list]
     # render the page by adding information to the index.html file
     return render_template('index.html', 
-                            avail_products=products,
-                            purchased_history=purchases)
+                            avail_products=products_and_price,
+                            purchased_history=purchase_name_and_price)
