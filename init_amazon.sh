eae59fee41ab1df16b940c2ac4c65bd6b8ff1a87
Rickard Stureborg <rs541@duke.edu>
2021-09-14 20:46:44 -0400

    implemented all db setup in single setup script

--- init_amazon.sh
+++ init_amazon.sh
@@ -2,18 +2,17 @@
 sudo apt update
 sudo apt install postgresql postgresql-contrib
 
-# Uncomment this if using venv
-# sudo apt install python3.8-venv
-# python3 -m venv env
-# source env/bin/activate
-# pip install -r requirements.txt
+# Remove this block if you don't want to use venv
+sudo apt install python3.8-venv
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
 
 # This process is interactive, you need to enter a password same as the one in config.py
 sudo -u postgres psql postgres -c '\password'
 
-
+# create the database
 dbname=amazon
-
 if [[ -n `sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -w "$dbname"` ]]; then
     sudo -u postgres dropdb $dbname
 fi
@@ -23,3 +22,5 @@ source env/bin/activate
 flask db init
 flask db migrate
 flask db upgrade
+
+# populate the database with initial data
