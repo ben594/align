9d043ea4f33d93c1b43bbe1ff289a30720476ab8
Rickard Stureborg <rs541@duke.edu>
2021-09-15 00:29:20 -0400

    fixed bug due to $ being misinterpreted by shell

--- init_amazon.sh
+++ init_amazon.sh
@@ -25,7 +25,7 @@ flask db upgrade
 
 # populate the database with initial data:
 # user 'icecream@tastes.good' has password 'test123' 
-psql amazon -c "INSERT INTO users(id,firstname,lastname,email,password_hash) VALUES (0,'Joey','Shmoey','icecream@tastes.good','pbkdf2:sha256:260000$1GvmeoAkcWb89TyU$5f711eafb243c1c1a884715dd9bd6d185f29ccd3dab59ad19cc201a7260091cb');"
+psql amazon -c "INSERT INTO users(id,firstname,lastname,email,password_hash) VALUES (0,'Joey','Shmoey','icecream@tastes.good','pbkdf2:sha256:260000\$1GvmeoAkcWb89TyU\$5f711eafb243c1c1a884715dd9bd6d185f29ccd3dab59ad19cc201a7260091cb');"
 # insert products
 psql amazon -c "INSERT INTO products(pid,product_name,price,available) VALUES (1,'vanilla ice cream',2.99,true);"
 psql amazon -c "INSERT INTO products(pid,product_name,price,available) VALUES (2,'chocolate ice cream',2.99,true);"
@@ -35,7 +35,7 @@ psql amazon -c "INSERT INTO products(pid,product_name,price,available) VALUES (5
 psql amazon -c "INSERT INTO products(pid,product_name,price,available) VALUES (6,'painting - the storm on the sea of galilee',44000000.00,true);"
 # insert previous purchases
 psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (0,0,1,'2021-09-10 13:12:58');"
-psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (0,0,2,'2021-09-11 13:17:23');"
-psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (0,0,1,'2021-09-12 13:11:36');"
-psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (0,0,2,'2021-09-13 13:19:01');"
-psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (0,0,3,'2021-09-13 19:18:54');"
+psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (1,0,2,'2021-09-11 13:17:23');"
+psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (2,0,1,'2021-09-12 13:11:36');"
+psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (3,0,2,'2021-09-13 13:19:01');"
+psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (4,0,3,'2021-09-13 19:18:54');"
