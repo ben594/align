b2b43468c1045cc1550d59e066e9cd43532a5e0f
yihaoh <yihaoh@bu.edu>
2021-09-17 23:59:42 -0400

    update init_amazon.sh so that it works on Google Cloud

--- init_amazon.sh
+++ init_amazon.sh
@@ -22,17 +22,17 @@ sudo -u postgres psql -af create.sql $dbname
 
 # populate the database with initial data:
 # user 'icecream@tastes.good' has password 'test123' 
-psql amazon -c "INSERT INTO users(email, password, firstname,lastname) VALUES ('icecream@tastes.good','pbkdf2:sha256:260000\$1GvmeoAkcWb89TyU\$5f711eafb243c1c1a884715dd9bd6d185f29ccd3dab59ad19cc201a7260091cb','Joey','Shmoey');"
+sudo -u postgres psql amazon -c "INSERT INTO users(id, email, password, firstname,lastname) VALUES (0,'icecream@tastes.good','pbkdf2:sha256:260000\$1GvmeoAkcWb89TyU\$5f711eafb243c1c1a884715dd9bd6d185f29ccd3dab59ad19cc201a7260091cb','Joey','Shmoey');"
 # insert products
-psql amazon -c "INSERT INTO products(pid,product_name,price,available) VALUES (1,'vanilla ice cream',2.99,true);"
-psql amazon -c "INSERT INTO products(pid,product_name,price,available) VALUES (2,'chocolate ice cream',2.99,true);"
-psql amazon -c "INSERT INTO products(pid,product_name,price,available) VALUES (3,'strawberry ice cream',2.99,true);"
-psql amazon -c "INSERT INTO products(pid,product_name,price,available) VALUES (4,'6-pack of paycheck pilsners',11.49,true);"
-psql amazon -c "INSERT INTO products(pid,product_name,price,available) VALUES (5,'seven fabergé easter eggs',1199000.00,true);"
-psql amazon -c "INSERT INTO products(pid,product_name,price,available) VALUES (6,'painting - the storm on the sea of galilee',44000000.00,true);"
+sudo -u postgres psql amazon -c "INSERT INTO products(pid,product_name,price,available) VALUES (1,'vanilla ice cream',2.99,true);"
+sudo -u postgres psql amazon -c "INSERT INTO products(pid,product_name,price,available) VALUES (2,'chocolate ice cream',2.99,true);"
+sudo -u postgres psql amazon -c "INSERT INTO products(pid,product_name,price,available) VALUES (3,'strawberry ice cream',2.99,true);"
+sudo -u postgres psql amazon -c "INSERT INTO products(pid,product_name,price,available) VALUES (4,'6-pack of paycheck pilsners',11.49,true);"
+sudo -u postgres psql amazon -c "INSERT INTO products(pid,product_name,price,available) VALUES (5,'seven fabergé easter eggs',1199000.00,true);"
+sudo -u postgres psql amazon -c "INSERT INTO products(pid,product_name,price,available) VALUES (6,'painting - the storm on the sea of galilee',44000000.00,true);"
 # insert previous purchases
-psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (0,0,1,'2021-09-10 13:12:58');"
-psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (1,0,2,'2021-09-11 13:17:23');"
-psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (2,0,1,'2021-09-12 13:11:36');"
-psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (3,0,2,'2021-09-13 13:19:01');"
-psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (4,0,3,'2021-09-13 19:18:54');"
+sudo -u postgres psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (0,0,1,'2021-09-10 13:12:58');"
+sudo -u postgres psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (1,0,2,'2021-09-11 13:17:23');"
+sudo -u postgres psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (2,0,1,'2021-09-12 13:11:36');"
+sudo -u postgres psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (3,0,2,'2021-09-13 13:19:01');"
+sudo -u postgres psql amazon -c "INSERT INTO purchases(order_nr,uid,pid,time_purchased) VALUES (4,0,3,'2021-09-13 19:18:54');"
