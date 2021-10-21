21df411b4a59d9982072e9b676f0692c12da2be9
Jun Yang <junyang@duke.edu>
2021-10-21 19:51:11 -0400

    add setting of sequence after loading database

--- db/load.sql
+++ db/load.sql
@@ -1,3 +1,6 @@
 \COPY Users FROM 'data/Users.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.users_id_seq',
+                         (SELECT MAX(id)+1 FROM Users),
+                         false);
 \COPY Products FROM 'data/Products.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Purchases FROM 'data/Purchases.csv' WITH DELIMITER ',' NULL '' CSV
