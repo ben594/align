2880827337c024ad47e71f1cf843f6bb3df000fb
Jun Yang <junyang@gmail.com>
2023-09-06 19:15:01 -0400

    fixed db login & flask upgrade

--- install.sh
+++ install.sh
@@ -6,12 +6,14 @@ cd $mybase
 
 SECRET=`tr -dc 'a-z0-9-_' < /dev/urandom | head -c50`
 echo "FLASK_APP=amazon.py
-FLASK_ENV=development
+FLASK_DEBUG=True
 FLASK_RUN_HOST=0.0.0.0
 FLASK_RUN_PORT=8080
 SECRET_KEY='$SECRET'
 DB_NAME=amazon
 DB_USER=${PGUSER}
+DB_PORT=${PGPORT}
+DB_HOST=${PGHOST}
 DB_PASSWORD=${PGPASSWORD}" > .flaskenv
 
 poetry config virtualenvs.in-project true
