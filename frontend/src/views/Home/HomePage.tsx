00ab76bb87dd6ffab9edbf91fb0d0a9ca7656e3d
ben594 <benli20042013@gmail.com>
2024-10-06 09:48:39 -0400

    Landing page

--- frontend/src/views/Home/HomePage.tsx
+++ frontend/src/views/Home/HomePage.tsx
@@ -1,7 +1,42 @@
+import { Box, Button, Image, Heading, Text, VStack } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
 export default function HomePage() {
-    return (
-        <div>
-            <h1>Hello there</h1>
-        </div>
-    );
+  const navigate = useNavigate();
+  const goToAuth = () => {
+    navigate("/auth");
+  };
+
+  return (
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Box flex="1" display="flex" justifyContent="center" alignItems="center">
+        <VStack spacing={10} align="flex-start">
+          <Heading size="3xl">
+            Fast
+            <br />
+            Accurate
+            <br />
+            Labels.
+          </Heading>
+          <Text color="gray">
+            Earn as you help vendors train their AI models.
+            <br />
+            The end-all solution to your date labeling needs.
+          </Text>
+          <Button colorScheme="blue" onClick={goToAuth}>
+            Get Started
+          </Button>
+        </VStack>
+      </Box>
+      <Box flex="1" display="flex" justifyContent="center" alignItems="center">
+        <Image padding="5vw" src={"/landingPageImage.png"} />
+      </Box>
+    </Box>
+  );
 }
