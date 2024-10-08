0cbdaca8434152a6ee50eb7affdd0f8d9556199e
ben594 <benli20042013@gmail.com>
2024-10-08 14:57:32 -0400

    Update controllers

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -18,7 +18,13 @@ def create_app():
     from .controllers.index import bp as index_bp
     app.register_blueprint(index_bp)
 
-    from .controllers.users import bp as user_bp
+    from .controllers.user_controller import bp as user_bp
     app.register_blueprint(user_bp)
+    
+    from .controllers.project_controller import project_bp
+    app.register_blueprint(project_bp)
+    
+    from .controllers.label_controller import label_bp
+    app.register_blueprint(label_bp)
 
     return app
