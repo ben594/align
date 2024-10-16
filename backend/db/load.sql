5995987f2f53c7ab6579abcc333def072a973aa2
Jamie Tan <jit7duke@gmail.com>
2024-10-16 15:35:11 -0400

    added some rough seed data

--- backend/db/load.sql
+++ backend/db/load.sql
@@ -1,4 +1,9 @@
 \COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Projects FROM 'Projects.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Payments FROM 'Payments.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Roles FROM 'Roles.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Images FROM 'Images.csv' WITH DELIMITER ',' NULL '' CSV
+
 -- since id is auto-generated; we need the next command to adjust the counter
 -- for auto-generation so next INSERT will not clash with ids loaded above:
 --SELECT pg_catalog.setval('public.users_id_seq',
