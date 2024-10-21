cf624be9567758d5a5a49699a7b4808529a4a39e
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-10-20 22:56:19 -0400

    Add ProjectCard and "start labeling" button

--- frontend/src/views/Project/ProjectDisplayPage.tsx
+++ frontend/src/views/Project/ProjectDisplayPage.tsx
@@ -1,27 +1,39 @@
-import { Box, Image, SimpleGrid } from '@chakra-ui/react'
+import { Box, Button, Image, SimpleGrid, Spinner } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 import { BACKEND_URL } from '../../constants'
+import FlexColumn from '../../components/FlexColumn'
+import FlexRow from '../../components/FlexRow'
 import Header from '../../components/Header'
 import ImageUploadWidget from './ImageUploadWidget'
+import { Project } from './ProjectCreationPage'
+import ProjectCard from '../../components/ProjectCard'
 import axios from 'axios'
-import { useParams } from 'react-router-dom'
 
 interface ProjectDisplayPageProps {
   projectId: string | undefined
 }
 
 export default function ProjectDisplayPage() {
+  const navigate = useNavigate()
   const { projectId } = useParams()
 
   const ImageGrid = ({ projectId }: ProjectDisplayPageProps) => {
     const [projectImages, setProjectImages] = useState<String[]>([])
+    const [project, setProject] = useState<Project>()
 
     useEffect(() => {
       const fetchImages = async () => {
+        const token = sessionStorage.getItem('jwt')
         try {
           const response = await axios.get(
-            `${BACKEND_URL}/project/${projectId}/images`
+            `${BACKEND_URL}/project/${projectId}/images`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
           )
           setProjectImages(response.data)
         } catch (error) {
@@ -31,12 +43,60 @@ export default function ProjectDisplayPage() {
       fetchImages()
     }, [projectId])
 
+    useEffect(() => {
+      const token = sessionStorage.getItem('jwt')
+      const fetchProject = async () => {
+        try {
+          const response = await axios.get(
+            `${BACKEND_URL}/projects/${projectId}`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          )
+          setProject(response.data)
+        } catch (error) {
+          console.error(error)
+        }
+      }
+      fetchProject()
+    }, [projectId])
+
     return (
-      <>
-        <ImageUploadWidget
-          projectId={projectId}
-          setProjectImages={setProjectImages}
-        />
+      <FlexColumn rowGap={4} alignItems="center">
+        <FlexColumn rowGap={4} maxWidth="1000px">
+          <FlexRow columnGap={4}>
+            {project ? (
+              <ProjectCard
+                name={project.name}
+                description={project.description}
+                id={project.id}
+                vendorUID={project.vendorUID}
+                pricePerImage={project.pricePerImage}
+                role={project.role}
+                hideButton={true}
+                height="min-content"
+                maxWidth="400px"
+              />
+            ) : (
+              <Spinner />
+            )}
+
+            <ImageUploadWidget
+              projectId={projectId}
+              setProjectImages={setProjectImages}
+            />
+          </FlexRow>
+          <Button
+            colorScheme="green"
+            onClick={() => {
+              navigate(`/label/${projectId}`)
+            }}
+          >
+            Start Labeling!
+          </Button>
+        </FlexColumn>
         <SimpleGrid columns={[2, null, 4]} spacing="40px">
           {projectImages.map((image_url: any, index: number) => (
             <Box key={index}>
@@ -44,7 +104,7 @@ export default function ProjectDisplayPage() {
             </Box>
           ))}
         </SimpleGrid>
-      </>
+      </FlexColumn>
     )
   }
 
