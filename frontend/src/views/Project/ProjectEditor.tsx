84df694426b342f0beb5bd4f6f7ae7defa93dc29
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-11-17 18:50:49 -0500

    Copy over from ProjectCreationPage

---
+++ frontend/src/views/Project/ProjectEditor.tsx
@@ -0,0 +1,168 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  Flex,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
+  Tag,
+  TagCloseButton,
+  TagLabel,
+  Textarea,
+  VStack,
+  useToast,
+} from '@chakra-ui/react'
+
+import { BACKEND_URL } from '../../constants'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+
+export default function ProjectEditor() {
+  const [projectName, setProjectName] = useState('')
+  const [description, setDescription] = useState('')
+  const [pricePerImage, setPricePerImage] = useState(0)
+  const [tag, setTag] = useState('')
+  const [tags, setTags] = useState<string[]>([])
+
+  const navigate = useNavigate()
+  const toast = useToast()
+
+  const handleAddTag = () => {
+    if (tag.trim() !== '') {
+      setTags([...tags, tag.toUpperCase()])
+      setTag('')
+    }
+  }
+
+  const handleRemoveTag = (indexToRemove: number) => {
+    setTags(tags.filter((_, index) => index !== indexToRemove))
+  }
+
+  const submitProject = async () => {
+    if (projectName === '' || description === '') {
+      toast({
+        title: 'Error',
+        description: 'Please fill out all required fields.',
+        status: 'error',
+      })
+      return
+    }
+
+    try {
+      const formData = new FormData()
+      formData.append('projectName', projectName)
+      formData.append('description', description)
+      formData.append('pricePerImage', pricePerImage.toString())
+      formData.append('tags', JSON.stringify(tags))
+
+      const token = sessionStorage.getItem('jwt')
+
+      const response = await axios.post(`${BACKEND_URL}/projects`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${token}`,
+        },
+      })
+
+      if (response.status === 201 || response.status === 200) {
+        toast({
+          title: 'Project created successfully!',
+          status: 'success',
+        })
+
+        navigate('/dashboard')
+      } else {
+        throw new Error('Failed to create project.')
+      }
+    } catch (error) {
+      toast({
+        title: 'Error',
+        description: 'Failed to create project.',
+        status: 'error',
+      })
+    }
+  }
+
+  return (
+    <Box>
+      <VStack spacing={5} width="100%" alignItems="flex-start">
+        <Card width="100%">
+          <CardBody>
+            <FormControl isRequired>
+              <FormLabel>Project Name</FormLabel>
+              <Input
+                type="text"
+                placeholder="Project Name"
+                value={projectName}
+                onChange={e => setProjectName(e.target.value)}
+              />
+            </FormControl>
+          </CardBody>
+          <CardBody>
+            <FormControl isRequired>
+              <FormLabel>Description</FormLabel>
+              <Textarea
+                placeholder="Describe your project"
+                value={description}
+                onChange={e => setDescription(e.target.value)}
+              />
+            </FormControl>
+          </CardBody>
+        </Card>
+
+        <Card width="100%">
+          <CardBody>
+            <FormControl isRequired>
+              <FormLabel>Price per Image (USD)</FormLabel>
+              <Input
+                type="number"
+                value={pricePerImage}
+                onChange={e =>
+                  setPricePerImage(e.target.value as unknown as number)
+                }
+              />
+            </FormControl>
+          </CardBody>
+        </Card>
+
+        <Card width="100%">
+          <CardBody>
+            <FormControl>
+              <FormLabel>Tags</FormLabel>
+              <Flex>
+                <Input
+                  type="text"
+                  value={tag}
+                  onChange={e => setTag(e.target.value)}
+                  placeholder="Provide a descriptive tag"
+                />
+                <Button colorScheme="blue" ml={2} onClick={handleAddTag}>
+                  Add
+                </Button>
+              </Flex>
+            </FormControl>
+
+            <HStack mt={4} spacing={2}>
+              {tags.map((tag, index) => (
+                <Tag key={index} variant="solid" size="sm">
+                  <TagLabel>{tag}</TagLabel>
+                  <TagCloseButton onClick={() => handleRemoveTag(index)} />
+                </Tag>
+              ))}
+            </HStack>
+          </CardBody>
+        </Card>
+
+        <Box>
+          <Button colorScheme="blue" width="150px" onClick={submitProject}>
+            Update
+          </Button>
+        </Box>
+      </VStack>
+    </Box>
+  )
+}
