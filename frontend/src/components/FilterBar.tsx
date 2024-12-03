31c82dea54078624ff24c03e758bef60f106cd84
rally.lin@duke.edu <linrally@gmail.com>
2024-12-02 22:16:45 -0500

    Added toggle for plaintext search vs tag filter

--- frontend/src/components/FilterBar.tsx
+++ frontend/src/components/FilterBar.tsx
@@ -1,5 +1,7 @@
-import {Menu, MenuButton, MenuList, MenuItemOption, Flex, IconButton, MenuOptionGroup } from '@chakra-ui/react'
-import { HamburgerIcon } from '@chakra-ui/icons';
+import {Menu, MenuButton, MenuList, MenuItemOption, Flex, IconButton, MenuOptionGroup, Input } from '@chakra-ui/react'
+import { FaTags } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
+import { MdOutlineSortByAlpha } from "react-icons/md";
 import {
   AutoComplete,
   AutoCompleteInput,
@@ -13,14 +15,18 @@ type FilterBarProps = {
     onFilterChange: (filters: string[]) => void,
     onTagRemoved: () => void,
     sortCriteria: string,
-    tagSet: Array<string>
+    tagSet: Array<string>,
+    mode: string,
+    onToggleMode: () => void,
+    searchText: string,
+    onSearchChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
 };
 
-export default function FilterBar({ onSortChange, sortCriteria, tagSet, onFilterChange, onTagRemoved }: FilterBarProps) {
+export default function FilterBar({ onSortChange, sortCriteria, tagSet, onFilterChange, onTagRemoved, mode, onToggleMode, searchText, onSearchChange }: FilterBarProps) {
     return (
         <Flex gap={4} paddingLeft={10} paddingRight={10}>
             <Menu closeOnSelect={false}>
-                <MenuButton as={IconButton} icon={<HamburgerIcon />} />
+                <MenuButton as={IconButton} icon={<MdOutlineSortByAlpha />} />
                 <MenuList>
                     <MenuOptionGroup value={sortCriteria} title='Sort By' type='radio'>
                         <MenuItemOption value="projectName" onClick={() => onSortChange("projectName")}>Project Name</MenuItemOption>
@@ -29,30 +35,43 @@ export default function FilterBar({ onSortChange, sortCriteria, tagSet, onFilter
                     </MenuOptionGroup>
                 </MenuList>
             </Menu>
-            <AutoComplete openOnFocus multiple onChange={onFilterChange} onTagRemoved={onTagRemoved}>
-                <AutoCompleteInput placeholder="Filter by tags...">
-                {({ tags }) =>
-                    tags.map((tag, tid) => (
-                    <AutoCompleteTag
-                        key={tid}
-                        label={tag.label}
-                        onRemove={tag.onRemove}
-                    />
-                    ))
-                }
-                </AutoCompleteInput>
-                <AutoCompleteList>
-                    {tagSet.map((tag) => (
-                        <AutoCompleteItem
-                            key={`option-${tag}`}
-                            value={tag}
-                            textTransform="capitalize"
-                        >
-                            {tag}
-                        </AutoCompleteItem>
-                    ))}
-                </AutoCompleteList>
-            </AutoComplete>
+            {
+                mode === "tags" ? (
+                    <>
+                        <AutoComplete openOnFocus multiple onChange={onFilterChange} onTagRemoved={onTagRemoved}>
+                            <AutoCompleteInput placeholder="Filter by tags...">
+                            {({ tags }) =>
+                                tags.map((tag, tid) => (
+                                <AutoCompleteTag
+                                    key={tid}
+                                    label={tag.label}
+                                    onRemove={tag.onRemove}
+                                />
+                                ))
+                            }
+                            </AutoCompleteInput>
+                            <AutoCompleteList>
+                                {tagSet.map((tag) => (
+                                    <AutoCompleteItem
+                                        key={`option-${tag}`}
+                                        value={tag}
+                                        textTransform="capitalize"
+                                    >
+                                        {tag}
+                                    </AutoCompleteItem>
+                                ))}
+                            </AutoCompleteList>
+                        </AutoComplete>
+                    <IconButton onClick={onToggleMode} icon={<FaTags/>} aria-label='Tags'/>
+                    </>
+                ) : 
+                (
+                    <>
+                        <Input value={searchText} onChange={onSearchChange} placeholder='Start typing...'/>
+                        <IconButton onClick={onToggleMode} icon={<FaSearch/>} aria-label='Search'/>
+                    </>
+                )
+            }
         </Flex>
     );
 }
